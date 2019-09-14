5b972087475f4fc9780c7233c117be0cb5d9d185
philipp012 <philipp.kiss@bluewin.ch>
2019-09-14 15:03:27 +0200

    finished

--- app/src/main/java/com/example/metaldetector/MainActivity.java
+++ app/src/main/java/com/example/metaldetector/MainActivity.java
@@ -1,19 +1,29 @@
 package com.example.metaldetector;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener{
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private TextView value;
     private SensorManager sensorManager;
@@ -28,6 +38,32 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         DECIMAL_FORMATTER = new DecimalFormat("#", symbols);
 
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        // get scan button
+        Button scanBtn = findViewById(R.id.scanButton);
+        scanBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+                intent.setPackage("com.google.zxing.client.android");
+                intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+                startActivityForResult(intent, 0);
+            }
+        });
+
+    }
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK) {
+                String contents = intent.getStringExtra("SCAN_RESULT");
+                String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+
+                log(contents);
+            } else if (resultCode == RESULT_CANCELED) {
+                // Handle cancel
+            }
+        }
     }
 
     @Override
@@ -65,5 +101,29 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    private void log(String logmessage) {
+        Intent intent = new Intent("ch.appquest.intent.LOG");
+
+        if (getPackageManager().queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY).isEmpty()) {
+            Toast.makeText(this, "Logbook App not Installed", Toast.LENGTH_LONG).show();
+            return;
+        }
+        JSONObject result = new JSONObject();
+        try {
+            result.put("task", "Metalldetektor");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        try {
+            result.put("solution", logmessage);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        intent.putExtra("ch.appquest.logmessage", result.toString());
+
+        startActivity(intent);
     }
 }
